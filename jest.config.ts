1fa88ac6ad5a2ac427f3b3b297ea27febf5201a4
Nandor Kraszlan <nandor.kraszlan@gmail.com>
2021-01-26 15:02:11 +0000

    Spooky jest is spooky

--- jest.config.ts
+++ jest.config.ts
@@ -8,31 +8,27 @@ export default {
     testMatch: [
         '**/?(*.)+(test).([tj]s?(x)|vue)'
     ],
-    moduleFileExtensions: ['js', 'ts', 'json', 'vue'],
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    moduleNameMapper: pathsToModuleNameMapper(compilerOptions?.paths ?? {}, { prefix: '<rootDir>' }),
-    'roots': [
-        '<rootDir>'
-    ],
+    // moduleNameMapper: pathsToModuleNameMapper(compilerOptions?.paths ?? {}, { prefix: '<rootDir>' }),
     transform: {
-        '^.+\\.js$': 'babel-jest',
         '^.+\\.vue$': 'vue-jest',
-        '^.+\\.[t]sx?$': 'ts-jest',
-        '.+\\.(css|styl|less|sass|scss|jpg|jpeg|png|svg|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-            'jest-transform-stub'
+        '^.+\\js$': 'babel-jest',
+        // '^.+\\.[t]sx?$': 'ts-jest',
+        // '.+\\.(css|styl|less|sass|scss|jpg|jpeg|png|svg|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+        //     'jest-transform-stub'
     },
-    transformIgnorePatterns: ['/node_modules/'],
-    snapshotSerializers: [
-        'jest-serializer-vue'
-    ],
-    globals: {
-        'vue-jest': {
-            // https://github.com/vuejs/vue-jest#global-jest-options
-            tsConfig: './tests/tsconfig.json'
-        },
-        'ts-jest': {
-            // https://kulshekhar.github.io/ts-jest/docs/options#options
-            tsconfig: './tests/tsconfig.json'
-        }
-    }
+    moduleFileExtensions: ['vue', 'js', 'json', 'jsx', 'ts', 'tsx', 'node'],
+    // transformIgnorePatterns: ['/node_modules/'],
+    // snapshotSerializers: [
+    //     'jest-serializer-vue'
+    // ],
+    // globals: {
+    //     'vue-jest': {
+    //         // https://github.com/vuejs/vue-jest#global-jest-options
+    //         tsConfig: './tests/tsconfig.json'
+    //     },
+    //     'ts-jest': {
+    //         // https://kulshekhar.github.io/ts-jest/docs/options#options
+    //         tsconfig: './tests/tsconfig.json'
+    //     }
+    // }
 };
--- tests/unit/components/HelloWorld.test.ts
+++ tests/unit/components/HelloWorld.test.ts
@@ -1,11 +1,12 @@
-import { shallowMount } from '@vue/test-utils';
+import { mount } from '@vue/test-utils';
 import HelloWorld from '../../../src/components/HelloWorld.vue';
 
 describe('HelloWorld.vue', () => {
     it('should render props.msg when passed', () => {
         const msg = 'new message';
-        const wrapper = shallowMount(HelloWorld, {
-            props: { msg }
+        const wrapper = mount(HelloWorld, {
+            props: { msg },
+            shallow: true
         });
         expect(wrapper.text()).toMatch(msg);
     });
